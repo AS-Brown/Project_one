80ac4eee73882094441139698659f49dd7d201b4
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-09-25 13:13:16 +0100

    Can select suppliers and products

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -1,8 +1,7 @@
 from db.run_sql import run_sql
 from models.product import Product
 from models.supplier import Supplier
-
-
+import repositories.supplier_repository as supplier_repository 
 
 def save_product(product):
     sql = "INSERT INTO products (name, description, buying_cost, sell_price, stock_count, type_of_product, supplier_id) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
@@ -10,4 +9,26 @@ def save_product(product):
     results = run_sql(sql, values)
     id = results[0]['id']
     product.id = id
+    return product
+
+def select_all_products():
+    products = []
+    sql = "SELECT * FROM products"
+    results = run_sql(sql)
+
+    for row in results:
+        supplier = supplier_repository.select_supplier(row['supplier_id'])
+        product = Product(row['title'], row['genre'], supplier, row['id'] )
+        products.append(product)
+    return products
+
+def select_product(id):
+    product = None
+    sql = "SELECT * FROM products WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        supplier = supplier_repository.select_supplier(result['supplier_id'])
+        product = Product(result['title'], result['genre'], supplier, result['id'] )
     return product
--- repositories/supplier_repository.py
+++ repositories/supplier_repository.py
@@ -10,4 +10,22 @@ def save_supplier(supplier):
     supplier.id = id
     return supplier
 
+def select_all_suppliers():
+    suppliers = []
+    sql = "SELECT * FROM suppliers"
+    results = run_sql(sql)
 
+    for result in results:
+        supplier = Supplier(result['name'], result['id'] )
+        suppliers.append(supplier)
+    return suppliers
+
+def select_supplier(id):
+    supplier = None
+    sql = "SELECT * FROM suppliers WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        supplier = Supplier(result['name'], result['id'] )
+    return supplier
