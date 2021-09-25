ee4e6d5b7081ba94fade7151eee516929481ed28
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-09-25 12:32:28 +0100

    Can save suppliers

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ from models.supplier import Supplier
 import repositories.product_repository as product_repository
 import repositories.supplier_repository as supplier_repository
 
-
+supplier_1 = Supplier("BI SMOLL")
+supplier_repository.save_supplier(supplier_1)
 
 pdb.set_trace()
---
+++ repositories/supplier_repository.py
@@ -0,0 +1,13 @@
+from db.run_sql import run_sql
+from models.supplier import Supplier
+
+
+def save_supplier(supplier):
+    sql = "INSERT INTO suppliers (name) VALUES (%s) RETURNING *"
+    values = [supplier.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    supplier.id = id
+    return supplier
+
+
