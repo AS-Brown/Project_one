69c77d5fe73b71b00db3b6c470318d2413800929
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-09-26 18:03:37 +0100

    Can create new supplier through website

--- controllers/supplier_controller.py
+++ controllers/supplier_controller.py
@@ -8,3 +8,15 @@ supplier_blueprint = Blueprint("suppliers", __name__)
 def suppliers():
     suppliers = supplier_repository.select_all_suppliers()
     return render_template("/suppliers/index.html", suppliers = suppliers)
+
+@supplier_blueprint.route('/suppliers/new')
+def new_supplier():
+    return render_template('/suppliers/new.html')
+
+@supplier_blueprint.route('/suppliers', methods=['POST'])
+def create_supplier():
+    name = request.form['name']
+    location = request.form['location']
+    supplier = Supplier(name, location)
+    supplier_repository.save_supplier(supplier)
+    return redirect('/suppliers')
