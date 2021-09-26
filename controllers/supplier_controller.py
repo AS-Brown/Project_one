73dfaadbcbc3d9d6162d920bc60e9158e8e9bebd
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-09-26 12:13:25 +0100

    Created '/suppliers' basic page

--- controllers/supplier_controller.py
+++ controllers/supplier_controller.py
@@ -4,3 +4,7 @@ import repositories.supplier_repository as supplier_repository
 
 supplier_blueprint = Blueprint("suppliers", __name__)
 
+@supplier_blueprint.route('/suppliers')
+def suppliers():
+    suppliers = supplier_repository.select_all_suppliers()
+    return render_template("/suppliers/index.html", suppliers = suppliers)
