7c61a49f0f30b26df5f00c4cb1ce8459e14f45d7
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-09-26 17:41:20 +0100

    Can create new product through website

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, Blueprint
 from models.product import Product
 import repositories.product_repository as product_repository
+import repositories.supplier_repository as supplier_repository
 
 product_blueprint = Blueprint("products", __name__)
 
@@ -8,3 +9,22 @@ product_blueprint = Blueprint("products", __name__)
 def products():
     products = product_repository.select_all_products()
     return render_template("/products/index.html", products = products)
+
+@product_blueprint.route('/products/new', methods = ['GET'])
+def new_product():
+    suppliers = supplier_repository.select_all_suppliers()
+    return render_template("products/new.html", suppliers = suppliers)
+
+@product_blueprint.route('/products', methods=['POST'])
+def create_product():
+    name = request.form['name']
+    description = request.form['description']
+    buying_cost = request.form['buying_cost']
+    sell_price = request.form['sell_price']
+    stock_count = request.form['stock_count']
+    type_of_product = request.form['type_of_product']
+    supplier_id = request.form['supplier_id']
+    supplier=supplier_repository.select_supplier(supplier_id)
+    product=Product(name, description, buying_cost, sell_price, stock_count, type_of_product, supplier, id)
+    product_repository.save_product(product)
+    return redirect('/products')
